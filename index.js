340a692a8e0a4bc1d3659762a0e701fe94ae5ef7
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-02-27 23:01:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ const port = process.env.PORT || 5000;
 
 app.get('/join', async (req, res) => {
   try {
-    const username = req.query.username || "YourCustomUsername"; // Use the provided username or set a custom one
     const bg = req.query.imagebackground;
     const groupname = req.query.groupname;
     const memberCount = req.query.count;
 
-    // Assuming you have a username parameter in the query
-    const usernameParameter = req.query.username;
+    // Assuming you have event.senderID and name parameters available
+    const senderID = req.query.senderID;
+    const username = req.query.name || "YourCustomUsername"; // Use the provided username or set a custom one
 
-    // Construct avatar URL using the provided username
-    const avatarUrl = `https://graph.facebook.com/${usernameParameter}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+    // Construct avatar URL using event.senderID
+    const avatarUrl = `https://graph.facebook.com/${senderID}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
 
     // Download the profile picture
     const avatarResponse = await axios.get(avatarUrl, { responseType: 'arraybuffer' });
 
     // Save the profile picture to a file
-    const avatarFilePath = __dirname + `/tmp/${usernameParameter}_avatar.jpg`;
+    const avatarFilePath = __dirname + `/tmp/${senderID}_avatar.jpg`;
     await fs.writeFile(avatarFilePath, Buffer.from(avatarResponse.data, 'binary'));
 
     const image = await new knights.Welcome2()
